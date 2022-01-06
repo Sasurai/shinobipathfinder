275c7b238fe908b3898a3adf01ffa593bd14ef43
Sasurai <sasurai.jmv@gmail.com>
2022-01-06 01:07:07 +0100

    Create project + NodeDataScriptable + Kiri
    Created Unity project.
    Created NodeDataScriptable + related classes (same file)
    Created Kiri & related Data objects, Kiri filled as sample

---
+++ Assets/Scripts/NodeDataScriptable.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/NodeDataScriptable", order = 1)]
+public class NodeDataScriptable : ScriptableObject
+{
+    public string name;
+    public Route[] routes;          
+}
+[Serializable]
+public class Route
+{
+    public NodeDataScriptable target;
+    public RouteData[] options;
+}
+
+[Serializable]
+public class RouteData
+{
+    public RouteType type;
+    public int days;
+    public int hours;
+    public int minutes;
+}
+
+public enum RouteType
+{
+    Walking,
+    Caravan,
+    Ninja,
+    Ship,
+    Train,
+    Dirigible
+}
