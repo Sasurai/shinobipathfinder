0aafb13816494bf2ce6a56e4202e684b3c634076
Sasurai <sasurai.jmv@gmail.com>
2022-01-06 13:52:11 +0100

    Route planning skeleton + comments (TODO)

--- Assets/Scripts/RoutePlanner.cs
+++ Assets/Scripts/RoutePlanner.cs
@@ -1,9 +1,27 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace ShinobiPathfinder
 {
+    public class RoutePlan : IComparable
+    {
+        public DateTime duration;
+        // TODO decide, origin will probably be implicit to avoid repetition
+        public List<NodeDataScriptable> route;
+
+        RoutePlan Clone()
+        {
+            var clone = new RoutePlan { duration = duration, route = new List<NodeDataScriptable>(route) };
+            return clone;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return duration.CompareTo(obj);
+        }
+    }
+
     public class RoutePlanner : MonoBehaviour
     {
         private NodeDataScriptable[] dataNodes;
@@ -17,7 +35,22 @@ namespace ShinobiPathfinder
 
         private void PlanRoute(NodeDataScriptable origin, NodeDataScriptable destination, TravelPreferences preferences)
         {
+            List<RoutePlan> plans = new List<RoutePlan>(10);
 
+            // Start from origin
+
+            // 1 possible route per exit from origin (picking option according to preferences)
+
+            // Iterate each route (probably easier to do this recursively)
+
+            // Discard routes if we hit a dead end (no other exits, only non-usable modes)
+
+            // Merge routes if they hit a common node and keep only shortest? This is good for pruning but may be expensive
+            // Alternative: keep list of visited nodes, discard if a route hits an already visited node (some other route already uses it, may lead to suboptimal routes)
+
+            // Sort plans and print shortest (in the future we may want to also display other N options?)
+
+            // Print error if no plan could be found with the given preferences
         }
     }
 }
